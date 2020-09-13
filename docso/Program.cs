092cb745c2dd5166139545cd27b2d9ce28f67d3f
ngocphuocpxu <70925960+ngocphuocpxu@users.noreply.github.com>
2020-09-13 21:18:54 +0700

    C#

---
+++ docso/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace docso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("CT ĐỌC SỐ ");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Nhập số (Nhập số 3 chữ số ): ");
+
+            int no = int.Parse(Console.ReadLine());
+            docso(no);
+        }
+        static void docso(int no)
+        {
+            string[] Ones = { "Một", "Hai", "Ba", "Bốn", "Năm", "Sáu", "Bảy", "Tám", "Chín", "Mười", "Mười một", "Mười hai", "Mười ba", "Mười bốn", "Mười lăm", "Mười sáu", "Mười bảy", "Mười tám", "Mười chín" };
+            string[] Tens = { "Mười", "Hai mươi", "Ba mươi", "Bốn mươi", "Năm mươi", "Sáu mươi", "Bảy mươi", "Tám mươi", "Chín mươi" };
+
+
+            string strWords = "";
+
+
+            if (no > 99 && no < 1000)
+            {
+                int i = no / 100;
+                strWords = strWords + Ones[i - 1] + " Trăm ";
+                no = no % 100;
+            }
+
+            if (no > 19 && no < 100)
+            {
+                int i = no / 10;
+                strWords = strWords + Tens[i - 1] + " ";
+                no = no % 10;
+            }
+
+            if (no > 0 && no < 20)
+            {
+                strWords = strWords + Ones[no - 1];
+            }
+
+            Console.WriteLine("Số của bạn là : {0}", strWords);
+        }
+    }
+}
